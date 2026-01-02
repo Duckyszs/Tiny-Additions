5760bd58b994c2688ef8f54768b6391e5d858579
Cmd <carlosmiguelisboar@gmail.com>
2026-01-02 19:07:03 -0300

    Initial commit

---
+++ src/main/java/net/Cmd/tinyadditions/TinyAdditionsClient.java
@@ -0,0 +1,10 @@
+package net.Cmd.tinyadditions;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TinyAdditionsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/Cmd/tinyadditions/TinyAdditionsMod.java
@@ -0,0 +1,16 @@
+package net.Cmd.tinyadditions;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TinyAdditionsMod implements ModInitializer {
+	public static final String MOD_ID = "tinyadditions";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
