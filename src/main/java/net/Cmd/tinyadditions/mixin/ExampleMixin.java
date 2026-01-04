3f8f7a17f13ecb030bebaeac5c89c2fbf47276b9
Cmd <carlosmiguelisboar@gmail.com>
2026-01-04 20:30:12 -0300

    Added Mixin

--- src/main/java/net/Cmd/tinyadditions/mixin/ExampleMixin.java
+++ src/main/java/net/Cmd/tinyadditions/mixin/ExampleMixin.java
@@ -1,15 +1,8 @@
 package net.Cmd.tinyadditions.mixin;
 
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.client.main.Main;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftServer.class)
+@Mixin(Main.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadWorld")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadWorld()V
-	}
 }
